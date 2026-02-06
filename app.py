e8728712a800f6b91e4b9b3cdda4e94e1cd16501
DanielC966 <danielc966@nycstudents.net>
2026-02-06 09:22:19 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+clothingStore = [
+    {
+        "item": "Blank Sweatpants",
+        "price": 40,
+        "size": "M",
+        "description": "Gray sweatpants",
+    },
+
+    {
+        "item": "Blank hoodie",
+        "price": 55,
+        "size": "L",
+        "description": "Black hoodie"
+    }, 
+
+    {
+        "item": "Designer hoodie",
+        "price": 800,
+        "size": "L",
+        "description": "Chrome Hearts hoodie"
+    }
+]
+
+print(clothingStore[2]["price"])
