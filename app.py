a037728292128419610c9c1d278d949b801ee28f
DanielC966 <danielc966@nycstudents.net>
2026-02-09 09:22:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-
 clothingStore = [
     {
-        "item": "Blank Sweatpants",
-        "price": 40,
-        "size": "M",
-        "description": "Gray sweatpants",
+        "item": {
+            "name": "Blank Sweatpants",
+            "price": 40,
+            "size": "M",
+            "description": "Gray sweatpants",
+        }
     },
 
     {
-        "item": "Blank hoodie",
-        "price": 55,
-        "size": "L",
-        "description": "Black hoodie"
+        "item1": {
+            "name":"Blank hoodie",
+            "price": 55,
+            "size": "L",
+            "description": "Black hoodie"
+        }
     }, 
 
     {
-        "item": "Designer hoodie",
-        "price": 800,
-        "size": "L",
-        "description": "Chrome Hearts hoodie"
+        "item2": { 
+            "name":"Designer hoodie",
+            "price": 800,
+            "size": "L",
+            "description": "Chrome Hearts hoodie"
+        }
     },
 ]
 
 # Part One
 
-print("Item Index: ")
-order = input("Which one item do you want to purchase?: ")
-print(clothingStore[order]["item"])
+order = input("Which one item do you want to purchase? (0-2): ")
+print(clothingStore[clothingStore.index(order)]["item"])
 
