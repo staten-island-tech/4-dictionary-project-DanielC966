6382e0c9a5f1c0fd0b7464ecde5589d724b88e02
DanielC966 <danielc966@nycstudents.net>
2026-02-11 09:25:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ clothingStore = [
     },
 
     {
-        "item": "Blank hoodie",
+        "item": "Blank Hoodie",
         "price": 55,
         "size": "L",
         "description": "Black hoodie"
     }, 
 
     {
-        "item": "Designer hoodie",
+        "item": "Designer Hoodie",
         "price": 800,
         "size": "L",
         "description": "Chrome Hearts hoodie"
@@ -23,7 +23,27 @@ clothingStore = [
 
 
 # Part One
-
+"""
 order = input("Which one item do you want to purchase? (0-2): ")
-print(clothingStore[clothingStore.index(order)]["item"])
+print(clothingStore[int(order)]["item"])
+"""
 
+# Part Two
+
+cart = []
+def showItemList():
+    print("Browse: ")
+    for i in range(len(clothingStore)):
+        print(f"Name: {clothingStore[i]["item"]} \n Price: {clothingStore[i]["price"]} \n Size: {clothingStore[i]["size"]} \n'{clothingStore[i]["description"]}' \n")
+
+wantToBuy = True
+
+while wantToBuy:
+    showItemList()
+    userPurchase = input("Which item would you like to purchase?: ")
+    cart.append(clothingStore[f"{userPurchase}"])
+    
+    confirm = input("Do you still want to continue browsing? y/n")
+    if confirm == "n":
+        break
+#userinput change to numerical***
